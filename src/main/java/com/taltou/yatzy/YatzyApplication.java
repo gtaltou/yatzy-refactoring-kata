4f15169a5591c1188d5e98777f31df0cbfa7722a
Guy Taltou <guy.taltou@gmail.com>
2024-03-09 18:57:03 +0100

    Initial Kata before refactoring

---
+++ src/main/java/com/taltou/yatzy/YatzyApplication.java
@@ -0,0 +1,13 @@
+package com.taltou.yatzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taltou/yatzy/YatzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taltou.yatzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
